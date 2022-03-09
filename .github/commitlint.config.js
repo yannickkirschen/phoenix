f05dc7a5e0d113fe452fe907baa853984e8fe924
Yannick Kirschen <yannickkirschen@protonmail.com>
2022-03-09 18:02:38 +0100

    feat: allow subject of commit messages to be sentence-case #27
    This will allow the subject of the commit message to start with an uppercase
    letter.
    
    Signed-off-by: Yannick Kirschen <yannickkirschen@protonmail.com>

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -7,7 +7,7 @@ module.exports = {
         'subject-case': [
             2,
             'never',
-            ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+            ['start-case', 'pascal-case', 'upper-case'],
         ],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
